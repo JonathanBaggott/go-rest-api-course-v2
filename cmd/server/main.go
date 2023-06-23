d6318642dba3f1bdbad4d910ee3a70776d9430e1
JonathanBaggott <112863867+JonathanBaggott@users.noreply.github.com>
2023-06-23 09:37:09 +0100

    adding migrations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/JonathanBaggott/go-rest-api-course-v2/internal/db"
+)
 
 // Run - is going to be responsible for the instantiation and startup of our Go application
 
 func Run() error {
 	fmt.Println("starting up our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database!")
+		return err
+	}
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate database")
+	}
+	fmt.Println("successfully connected and pinged database")
 	return nil
 }
 
